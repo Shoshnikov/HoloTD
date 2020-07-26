80b71839cbcea0d590584a4fb714bf799efbefa8
Mixail <mixail.korchikov@mail.ru>
2020-07-26 16:53:35 +0300

    Wrong Anchor Sharing

--- Assets/Scripts/Net/TableScaler.cs
+++ Assets/Scripts/Net/TableScaler.cs
@@ -3,32 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.XR.WSA.Sharing;
+using Microsoft.MixedReality.Toolkit.Experimental.Utilities;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Persistence;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PhotonView))]
 
-public class TableScaler : MonoBehaviour
+public class TableScaler : MonoBehaviourPunCallbacks
 {
     [SerializeField] [Range(1, 90)] private byte rotationAngle = 1;
     [SerializeField] [Range(0.001f, 0.1f)] private float scaleIndex = 0.1f;
     [SerializeField] [Range(0, 5)] private float differenceIndex = 0.2f;
     [SerializeField] private Transform[] Ray;
     private Transform transform;
-
-    private Vector3 down = new Vector3(0, -1f, 0);
+    private PhotonView photonView;
+    private WorldAnchorManager worldAnchorManager = new WorldAnchorManager();
+    private WorldAnchorStore worldAnchorStore;
+    private bool savedAnchor;
+    private int retryCount = 10;
+    public byte[] fileData { get; private set; }
 
     private void Awake()
     {
         transform = gameObject.GetComponent<Transform>();
+        photonView = gameObject.GetComponent<PhotonView>();
     }
 
-    private void Start()
+    public void Table(int w8Time)
     {
-        Invoke("DropTable", 15);
+        Invoke("DropTable", w8Time);
     }
 
-    public void DropTable()
+    private void DropTable()
     {
-        Debug.Log("Table teleport");
         transform.position = CoreServices.InputSystem.GazeProvider.HitInfo.point;  //Отрпавляет стол на положение гейза.
         transform.Translate(Vector3.up * 0.03f);
         ScaleTable();
@@ -63,18 +75,19 @@ public class TableScaler : MonoBehaviour
 
         transform.rotation = onAngleMaxScale;
         transform.localScale = MaxScale;
+        ExportWorldAnchor();
     }
 
     private void Update()
     {
-        TestRays();
+        //TestRays();
     }
 
     private void UpdRays(ref Ray[] rays)
     {
         for (int i = 0; i < Ray.Length; ++i)
         {
-            rays[i] = new Ray(Ray[i].position, down);
+            rays[i] = new Ray(Ray[i].position, Vector3.down);
         }
     }
 
@@ -110,9 +123,71 @@ public class TableScaler : MonoBehaviour
 
         for (int i = 0; i < Ray.Length; ++i)
         {
-            rays[i] = new Ray(Ray[i].position, down);
+            rays[i] = new Ray(Ray[i].position, Vector3.down);
             Physics.Raycast(rays[i], out hits[i]);
             Debug.DrawRay(Ray[i].position, rays[i].direction * hits[i].distance, Color.red);
         }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Table(7);
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        worldAnchorManager.RemoveAnchor(this.gameObject);
+    }
+
+    [PunRPC] private void SendAnchor(byte[] data)
+    {
+        Debug.Log($"Якорь принят {data}");
+        fileData = data;
+        WorldAnchorTransferBatch.ImportAsync(data, OnImportComplete);
+    }
+
+    private void OnImportComplete(SerializationCompletionReason completionReason, WorldAnchorTransferBatch deserializedTransferBatch)
+    {
+        if (completionReason != SerializationCompletionReason.Succeeded)
+        {
+            Debug.Log("Failed to import: " + completionReason.ToString());
+            if (retryCount > 0)
+            {
+                retryCount--;
+                WorldAnchorTransferBatch.ImportAsync(fileData, OnImportComplete);
+            }
+            return;
+        }
+        deserializedTransferBatch.LockObject("TableAnchor", gameObject);
+    }
+
+
+    private void ExportWorldAnchor()   //Wrong
+    {
+        worldAnchorManager.AttachAnchor(this.gameObject, "TableAnchor");
+        WorldAnchor MyWorlAnchor = worldAnchorManager.AnchorStore.Load("TableAnchor", this.gameObject);
+        WorldAnchorTransferBatch transferBatch = new WorldAnchorTransferBatch();
+        transferBatch.AddWorldAnchor("GameRootAnchor", MyWorlAnchor);
+        WorldAnchorTransferBatch.ExportAsync(transferBatch, OnExportDataAvailable, OnExportComplete);
+    }
+
+    private void OnExportComplete(SerializationCompletionReason completionReason)
+    {
+        if (completionReason == SerializationCompletionReason.Succeeded)
+        {
+            Debug.Log("Якорь был сериализован");
+        }
+        else
+        {
+            Debug.Log("Якорь не был сериализован");
+        }
+    }
+
+    private void OnExportDataAvailable(byte[] data)
+    {
+        photonView.RPC("SendAnchor", RpcTarget.OthersBuffered, data);
     }
 }
